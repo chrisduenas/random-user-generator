186f2bcaff312da54876b59f1e67c84206b5bfe3
Christopher Duenas <chrisduenasonline@gmail.com>
2020-03-28 11:30:31 -0400

    adding api to react code

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 class App extends React.Component {
+    componentDidMount() {
+        axios.get('http://localhost:3000/users')
+             .then(res => {
+                 console.log(res.data);
+             })
+    }
+
     render() {
         return (
             <h1>Welcome to React!</h1>
