ed0501e0023f06feaa6564ff8da20dae5ca116ab
Christopher Duenas <chrisduenasonline@gmail.com>
2020-03-28 11:29:45 -0400

    Added random users api to index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
 
 app.get('/', (req,res) => {
     res.send("<h1> Home Page </h1>");
+});
+
+app.get('/users', (req,res) => {
+    axios.get('https://randomuser.me/api/?page=1&results=10')
+         .then(response => {
+             res.send(response.data);
+         });
 });
 
 app.listen(3000, () => {
