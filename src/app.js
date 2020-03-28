d9da9e4cb8688391a145e60532d95cdc4ff78846
Christopher Duenas <chrisduenasonline@gmail.com>
2020-03-28 11:49:53 -0400

    Adding store dispatch to send user data to react file

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import store from './store/store';
+import { addUsers } from './actions/users';
 
 class App extends React.Component {
     componentDidMount() {
         axios.get('http://localhost:3000/users')
              .then(res => {
                  console.log(res.data);
+                 store.dispatch(addUsers(response.data.results));
              })
     }
 
