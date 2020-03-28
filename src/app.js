f1786670c7ed43bae82f3767e9c80c7b2180e3ff
Christopher Duenas <chrisduenasonline@gmail.com>
2020-03-28 11:56:45 -0400

    added header component to app

--- src/app.js
+++ src/app.js
@@ -3,19 +3,22 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 import store from './store/store';
 import { addUsers } from './actions/users';
+import Header from './components/Header'
 
 class App extends React.Component {
     componentDidMount() {
         axios.get('http://localhost:3000/users')
              .then(res => {
-                 console.log(res.data);
+                //  console.log(res.data);
                  store.dispatch(addUsers(res.data.results));
              })
     }
 
     render() {
         return (
-            <h1>Welcome to React!</h1>
+            <div>
+                <Header />
+            </div>
         )
     }
 }
